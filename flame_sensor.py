b7b0b7c29595d79d929c1e684a9156c6c7fac8b8
Mathis <mathis.teix@gmail.com>
2023-05-23 10:54:33 +0200

    Add flame_sensor

---
+++ flame_sensor.py
@@ -0,0 +1,14 @@
+import RPI.GPIO as GPIO
+from time import sleep
+
+flame_pin = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(flame_pin, GPIO.IN)
+
+def callback(channel):
+    print("Flame detected")
+
+GPIO.add_event_detect(flame_pin, GPIO.BOTH, callback=callback, bouncetime=300)
+
+while True:
+    time.sleep(1)
