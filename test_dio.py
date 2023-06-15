404e0e63312f81cd7c2ed59c4e6c6898d802081c
Mathis <mathis.teix@gmail.com>
2023-06-15 13:56:05 +0200

    .

--- test_dio.py
+++ test_dio.py
@@ -66,6 +66,7 @@ def main():
     while True:
         if i > 4:
             ed.set_dio_mapping(DIO_RX)
+            ed.set_mode(MODE.STBY)
             ed.reset_ptr_rx()
             ed.set_mode(MODE.RXCONT)
 
